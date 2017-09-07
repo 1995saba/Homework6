f14f821994e9607bc882a144cce877e5a3d223b3
Sabina <sabina.orunbaeva@gmail.com>
2017-09-07 09:16:41 +0600

    Add files via upload

---
+++ Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("Введите количество элементов массива: ");
+            string sizeAsString = Console.ReadLine();
+            bool result = Int32.TryParse(sizeAsString, out size);
+            if (!result)
+            {
+                Console.WriteLine("Attempted conversion of '{0}' failed.",
+                                   sizeAsString == null ? "<null>" : sizeAsString);
+            }
+
+            SquaredArray array = new SquaredArray(size);
+
+            for (int i = 1; i < size; i++)
+            {
+                array[i] = i;
+                Console.WriteLine("array[" + i + "] = " + array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework/SquaredArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class SquaredArray
+    {
+        private int[] number;
+        public int this[int index]
+        {
+            get
+            {
+                return number[index];
+            }
+            set
+            {
+                number[index] = (value * value);
+            }
+        }
+
+        public SquaredArray(int size)
+        {
+            number = new int[size];
+        }
+    }
+}
